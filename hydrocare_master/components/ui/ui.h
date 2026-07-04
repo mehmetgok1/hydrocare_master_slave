55bb6db53960a237a9dc974204e6f349936dcc4b
mehmet.gok1 <mehmetgk2015@gmail.com>
2026-07-05 00:30:39 +0300

    ui is built

--- hydrocare_master/components/ui/ui.h
+++ hydrocare_master/components/ui/ui.h
@@ -1,10 +1,9 @@
 #ifndef LED_H
 #define LED_H
 #include "config.h"
+#include "freertos/projdefs.h"
 
-void uiInit(uint8_t *red, uint8_t *green, uint8_t *blue);
 void uiChargingScenario(uint8_t *red, uint8_t *green, uint8_t *blue);
-void setLED(uint8_t *red, uint8_t *green, uint8_t *blue);
 void uiOTAStarted(uint8_t *red, uint8_t *green, uint8_t *blue);
 
 #endif
