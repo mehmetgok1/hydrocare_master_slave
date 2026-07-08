09b24016e578575dc6f5dd6dc9fecfd7abb71714
mehmet.gok1 <mehmetgk2015@gmail.com>
2026-07-08 22:40:35 +0300

    debug codes removed

--- hydrocare_master/main/main.c
+++ hydrocare_master/main/main.c
@@ -5,7 +5,7 @@
 #include <inttypes.h> // Include for portable format specifiers like PRIu32
 
 const char *TAG2 = "MAIN";
-bool debug_infos = true; // Set to true to enable detailed debug prints*/
+bool debug_infos = false; // Set to true to enable detailed debug prints*/
 
 volatile bool timerStream = false;
 uint16_t downsampled16x16[256];  // Shared with BLE for transmission
